54bc0060baf299b25b121bdae426c1c9fb111e63
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-18 22:00:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(greeting)
   
   if greeting == "I LOVE YOU GRANDMA!"
-    p "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elseif greeting.upcase
-    p "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   else
-    p "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 
 end
