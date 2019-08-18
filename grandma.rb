b15647cab6b59b69f09b89fb5089059b4165f376
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-18 23:39:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
+  if phrase == phrase.upcase
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif ("phrase")
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
+  else phrase.upcase
     puts "NO, NOT SINCE 1938!"
 
 end
